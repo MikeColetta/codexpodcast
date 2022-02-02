645398dfd97a2e10a22f081a1d06323a99f1ea80
Mike Coletta <coletta.mike@gmail.com>
2022-02-01 21:20:44 -0800

    Updated CSS

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,30 +17,31 @@ class About extends React.Component {
                         </h3>
                     </Row>
 
-                    <Row style={{ color: 'white' }}>
-                        Mike Coletta and Tyler Ostby talk about the history of video games. Originally using the Ultimate History of Video Games Vol. 1 as a source.
-                        They've branched off to cover different games, books and movies related to video games. 
+                    <Row className="p-3" style={{ color: 'white' }}>
+                        Mike Coletta and Tyler Ostby talk about the history of video games. They started from the beginning using the Ultimate History of Video Games
+                        Vol. 1 by Steven L. Kent. as a source. Since then they've branched off to cover the history of individual games, consoles, and moments in video
+                        game history! Other books covered include Masters of Doom by David Kushner and The Ultimate History of Video Games Vol. 2 by Steven L. Kent.
                     </Row>
-                    <Card className="mt-3 aboutCard">
+                    <Card className="mt-3 p-3 aboutCard">
                         <Row className="mt-3 aboutRow">                  
                             <Col md className="aboutCol">
-                                Mike Coletta is a Seattle based comedian originally from Alaska. When not doing stand-up he loves video games, Dungeons and Dragons,
+                                <h5>Mike Coletta is a Seattle based comedian originally from Alaska. When not doing stand-up he loves video games, Dungeons and Dragons,
                                 and annoying his cat Delilah.
                                 Coletta has performed at The Bridgetown Comedy Festival, Emerald City Comicon, Treefort Fest, and Bumbershoot.
-                                He's also the co-host of the HugLife Podcast.
+                                He's also the co-host of the HugLife Podcast.</h5>
                             </Col>
-                            <Col md className="aboutCol">
+                            <Col md xl={4} className="aboutCol">
                                 <Image className="aboutImage" fluid='true' rounded='true' src={MikeHeadshot} alt="Mike Coletta" />
                             </Col>
                         </Row>
                     </Card>
-                    <Card className="mt-3 aboutCard">
+                    <Card className="mt-3 p-3 aboutCard">
                         <Row className="mt-3 aboutRow">
-                            <Col md className="aboutCol">
+                            <Col md xl={4} className="aboutCol">
                                 <Image fluid='true' rounded='true' src={TylerHeadshot} alt="Mike Coletta" />
                             </Col>
                             <Col md className="aboutCol">
-                                Tyler Ostby is one of the greatest humans alive and a J-Bird champion.
+                                <h5>Tyler Ostby is one of the greatest humans alive and a J-Bird champion.</h5>
                             </Col>
                         </Row>
                     </Card>
