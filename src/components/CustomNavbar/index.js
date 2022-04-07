d33542648232015631c71a705b911cc568ff605f
Mike Coletta <coletta.mike@gmail.com>
2022-04-06 20:44:05 -0700

    Added dropdown menu

--- src/components/CustomNavbar/index.js
+++ src/components/CustomNavbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom"
 import "./style.css";
 import Logo from '../../assets/codexLogoSM.jpg'
@@ -7,30 +7,26 @@ import Logo from '../../assets/codexLogoSM.jpg'
 function CustomNavbar() {
     return (
         <Navbar className="customNavbar" expand="lg" variant="dark">
-                <Navbar.Brand href="#home">
-                    <Link to="/">
-                        <img
-                            alt="Codex Logo"
-                            src={Logo}
-                            width="60"
-                            height="60"
-                            className="d-inline-block align-top navbarLogo"
-                        />{' '}
-                    </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        <Link to="/About" className="navbarItem">
-                            About
-                        </Link>
-                    </Navbar.Text>
-                    <Navbar.Text>
-                        <Link to="/Contact" className="navbarItem">
-                            Contact
-                        </Link>
-                    </Navbar.Text>
-                </Navbar.Collapse>
+            <Navbar.Brand href="#home">
+                <Link to="/">
+                    <img
+                        alt="Codex Logo"
+                        src={Logo}
+                        width="60"
+                        height="60"
+                        className="d-inline-block align-top navbarLogo"
+                    />{' '}
+                </Link>
+            </Navbar.Brand>
+            <NavDropdown title="Menu" id="nav-dropdown" className='dropdown'>
+                <Link to="/About" className="navbarItem">
+                    About
+                </Link>
+                <br></br>
+                <Link to="/Contact" className="navbarItem">
+                    Contact
+                </Link>
+            </NavDropdown>
         </Navbar>
     )
 }
