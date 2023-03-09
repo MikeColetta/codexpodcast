a960bd7804c575fcbcd05acee65ff2889bf9bcb0
Mike Coletta <coletta.mike@gmail.com>
2023-03-08 18:13:05 -0800

    Reformatted page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContactForm from '../components/ContactForm';
-import {Row, Container} from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
 
 
 class Contact extends React.Component {
@@ -9,15 +9,15 @@ class Contact extends React.Component {
         return (
             <div>
                 <Container>
-                <Row>
-                    <h3 className="recentHeading mt-1">
-                        Contact Us!
-                    </h3>
-                </Row>
+                    <Row>
+                        <h3 className="recentHeading mt-1">
+                            Contact Us!
+                        </h3>
+                    </Row>
 
-                <Row style={{ color: '#FAF3DD', justifyContent: 'center' }}>
-                    Have a question for the show? Send us an email below or at codexhistorypodcast@gmail.com!
-                </Row>
+                    <Row style={{ color: '#FAF3DD', justifyContent: 'center' }}>
+                        Have a question for the show? Send us an email below or at codexhistorypodcast@gmail.com!
+                    </Row>
                 </Container>
                 <ContactForm />
             </div>
