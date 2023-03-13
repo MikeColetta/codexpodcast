6c5555c58816d62f3e6b09a4e479873e9a58ba96
Mike Coletta <coletta.mike@gmail.com>
2023-03-12 21:25:09 -0700

    New version of Codex Pong

--- src/pages/CodexPong.js
+++ src/pages/CodexPong.js
@@ -13,8 +13,11 @@ class Contact extends React.Component {
                             Codex Pong - First to 3 points wins!
                         </h3>
                     </Row>
+                    <Row style={{ color: '#FAF3DD', justifyContent: 'center' }}>
+                        *WARNING - Game not yet optimized for mobile devices.
+                    </Row>
                     <Row>
-                        <iframe frameborder="0" src="https://itch.io/embed-upload/7479314?color=333333" allowfullscreen="true" width="640" height="380" title='codexpong'>
+                        <iframe frameborder="0" src="https://itch.io/embed-upload/7510374?color=333333" allowfullscreen="" width="960" height="570" title='codexpong'>
                             <a href="https://catamaran.itch.io/codex-pong">Play Codex Pong on itch.io</a>
                         </iframe>
                     </Row>
@@ -22,14 +25,13 @@ class Contact extends React.Component {
                         <br></br>
                         Controls:
                         <br></br>
-                        Up/Down Arrows - Move paddle up and down
+                        W - Move paddle up
+                        <br></br>
+                        S - Move paddle down
                         <br></br>
                         Space Bar - Reset game
                         <br></br>
-                        B - Ball reset (in case you get stuck)
-                        <br></br>
-                        <br></br>
-                        *WARNING - Game not yet optimized for mobile devices.
+                        B - Ball reset (in case ball gets stuck)
                     </Row>
                 </Container>
             </div>
